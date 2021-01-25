7cb2b05e11b9e7cba61b7887896007f986b4a150
andriyandriy1979 <Pukhovych1979@gmail.com>
2021-01-25 15:13:53 +0000

    Sample code

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function menuburg() {
+	display = document.getElementById('blockburgmenu').style.display;
+	if (display === "none") {
+		document.getElementById('blockburgmenu').style.display = "block";
+	} else {
+		document.getElementById('blockburgmenu').style.display = "none";
+	}
+}
+
